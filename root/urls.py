943c7661f8dc443d79b1ea8b86c2ae32de3d7501
krv006 <rvkamronbek@gmail.com>
2025-04-17 21:32:05 +0500

    settings

--- root/urls.py
+++ root/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
