25e7552fab7bd39d2292dbc28089d2b7fc319c7d
krv006 <rvkamronbek@gmail.com>
2025-04-18 14:38:39 +0500

    model and serializer

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.views import ProductListCreateAPIView, CategoryListCreateAPIView
+from apps.views import ProductListCreateAPIView, CategoryListCreateAPIView, UserListApiView
 
 urlpatterns = [
-    path('category/', CategoryListCreateAPIView.as_view(), name='product_list'),
+    path('user/', UserListApiView.as_view(), name='user_list'),
+    path('category/', CategoryListCreateAPIView.as_view(), name='category_list'),
     path('product/', ProductListCreateAPIView.as_view(), name='product_list'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,14 +1,23 @@
-from rest_framework.generics import ListCreateAPIView
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import ListCreateAPIView, ListAPIView
 
-from apps.models import Category, Product
-from apps.serializers import CategoryModelSerializer, ProductModelSerializer
+from apps.models import Category, Product, User
+from apps.serializers import CategoryModelSerializer, ProductModelSerializer, UserModelSerializer
 
 
+@extend_schema(tags=['User'])
+class UserListApiView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+
+
+@extend_schema(tags=['Product'])
 class CategoryListCreateAPIView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryModelSerializer
 
 
+@extend_schema(tags=['Product'])
 class ProductListCreateAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializer
