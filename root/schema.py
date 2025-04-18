8874c0856bb1fbb11ecd30f9b3b3bf6885a59b64
krv006 <rvkamronbek@gmail.com>
2025-04-18 15:37:01 +0500

    graphql

--- root/schema.py
+++ root/schema.py
@@ -1,8 +1,30 @@
-from graphene import ObjectType, String, Schema
+import graphene
+from graphene_django import DjangoObjectType
+from apps.models import Category, Product
 
 
-class Query(ObjectType):
-    hello = String(default_value="Hi!")
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
 
 
-schema = Schema(query=Query)
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'discount_price', 'short_description', 'long_description',
+                  'stock', 'category', 'tags', 'is_available', 'image')
+
+
+class Query(graphene.ObjectType):
+    all_categories = graphene.List(CategoryType)
+    all_products = graphene.List(ProductType)
+
+    def resolve_all_categories(root, info):
+        return Category.objects.all()
+
+    def resolve_all_products(root, info):
+        return Product.objects.all()
+
+
+schema = graphene.Schema(query=Query)
