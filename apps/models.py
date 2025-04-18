7560a8553aa36e8d942cef72b455fa18472af129
krv006 <rvkamronbek@gmail.com>
2025-04-18 12:13:10 +0500

    change model add

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Model, CharField, DecimalField, TextField, ImageField, CASCADE, ForeignKey, BooleanField, \
+    PositiveIntegerField
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Category(Model):
+    name = CharField(max_length=100)
+
+
+class Product(Model):
+    name = CharField(max_length=100)
+    price = DecimalField(decimal_places=2, max_digits=10)
+    discount_price = DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    short_description = TextField()
+    long_description = TextField()
+    image = ImageField(upload_to='products/images/')
+    stock = PositiveIntegerField(default=0)
+    is_available = BooleanField(default=True)
+    category = ForeignKey(Category, CASCADE, related_name='products')
+    tags = CharField(max_length=255, blank=True, help_text="Taglarni vergul bilan ajrating (e.g. summer,new)")
+
+    def get_final_price(self):
+        return self.discount_price if self.discount_price else self.price
+
+    def __str__(self):
+        return f"{self.name} - ${self.get_final_price()}"
--- root/settings.py
+++ root/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,6 +35,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'root.urls'
+AUTH_USER_MODEL = 'apps.User'
 
 TEMPLATES = [
     {
@@ -77,14 +79,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# todo DRF settings
 from root.drf_settings import *
